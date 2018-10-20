c07485242c88594689b7f2620b586bc87ef01fd2
Erwan ROUSSEL <erwan.roussel@gmail.com>
2018-10-20 10:47:24 +0200

    Add styled component test

---
+++ tests/styled-component.test.js
@@ -0,0 +1,25 @@
+const { Component } = require('../eazer')
+
+test('return a styled component', () => {
+  const Title = {
+    type: 'h1',
+    style: 'color: blue'
+  }
+
+  const Paragraph = {
+    type: 'p'
+  }
+
+  const aliases = {
+    MyComponentTitle: Title,
+    MyComponentParagraph: Paragraph
+  }   
+
+  const content = {
+    MyComponentTitle: 'Super component',
+    MyComponentParagraph: 'Super paragraph'
+  }
+
+  const component = new Component('MySuperComponent', content, aliases, `font-family: Arial`)
+  expect(component.render()).toBe(`<div class='MySuperComponent' style="font-family: Arial"><h1   style="color: blue">Super component</h1></div>`)
+})
